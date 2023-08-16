c3dda2a12e121056af58a3efe8d700caa4335902
Mariana Casarin <mariana.battistin@gmail.com>
2023-08-16 11:43:37 +0100

    Creation of application DbContext and Category model.

---
+++ ExampleECommerceProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ExampleECommerceProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace ExampleECommerceProject.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            ServerVersion serverVersion = ServerVersion.AutoDetect(connectionString);
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- ExampleECommerceProject/Program.cs
+++ ExampleECommerceProject/Program.cs
@@ -1,10 +1,12 @@
+using ExampleECommerceProject.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
