b32ea254c8d6c98202157b9efa5675e31cedcc70
Mariana Casarin <mariana.battistin@gmail.com>
2023-08-24 17:47:05 +0100

    Creation of category crud with model, views and controller.

--- ExampleECommerceProject/Models/Category.cs
+++ ExampleECommerceProject/Models/Category.cs
@@ -6,8 +6,12 @@ namespace ExampleECommerceProject.Models
     {
         [Key]
         public int Id { get; set; }
+        
         [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        
+        [Range(1,100, ErrorMessage ="Display order must be between 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
